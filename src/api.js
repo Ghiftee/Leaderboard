81ee7b1b446316c8c827850b2883ff67fa4ccc70
Ghiftee <gigibetine@gmail.com>
2021-07-28 16:10:56 +0100

    Fix eslint errors

--- src/api.js
+++ src/api.js
@@ -6,19 +6,18 @@ async function createGame() {
 
   // If you haven't, create a new game
   const game = {
-    "name": "Awesome Game"
+    name: 'Awesome Game',
   };
 
-  const response = await fetch(`${baseUrl}games/`, 
+  const response = await fetch(`${baseUrl}games/`,
     {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(game),
     });
-  
-  let data =  await response.json();
+  const data = await response.json();
   const gameId = data.result.split(' ')[3];
   localStorage.setItem('gameId', gameId);
 }
@@ -27,10 +26,9 @@ async function refresh() {
   const gameId = localStorage.getItem('gameId');
   const list = document.getElementById('scores');
   list.innerHTML = '';
-  
   const response = await fetch(`${baseUrl}games/${gameId}/scores`);
   const scores = await response.json();
-  scores.result.forEach(score => {
+  scores.result.forEach((score) => {
     const li = document.createElement('li');
     li.innerText = `${score.user}: ${score.score}`;
     list.append(li);
@@ -45,23 +43,21 @@ async function submit(e) {
   const scoreElement = document.getElementById('scoreInput');
 
   const newScore = {
-    "user": nameELement.value,
-    "score": scoreElement.value,
+    user: nameELement.value,
+    score: scoreElement.value,
   };
-
   const response = await fetch(`${baseUrl}games/${gameId}/scores`, 
     {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(newScore),
     });
 
   const data = await response.json();
-  alert(data.message || data.result);
   nameELement.value = '';
   scoreElement.value = '';
 }
 
-module.exports = {createGame, refresh, submit}
+module.exports = { createGame, refresh, submit }
